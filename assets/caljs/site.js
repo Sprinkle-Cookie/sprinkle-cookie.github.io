7597e93be97694be2e2bb39324af4e0ce749886e
Nandaja <nandaja.varma@gmail.com>
2018-04-04 16:01:20 +0200

    april

--- assets/caljs/site.js
+++ assets/caljs/site.js
@@ -44,7 +44,7 @@ Calendar.prototype.generateHTML = function(){
       monthLength = 29;
     }
   }
-  var theDayWeMeet = new Date(2018, 02, 23);
+  var theDayWeMeet = new Date(2018, 03, 28);
 
   // Fill in month and year for header
   var monthName = cal_months_labels[this.month];
